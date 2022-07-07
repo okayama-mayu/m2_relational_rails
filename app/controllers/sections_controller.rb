166a21369d0207959930ea2227733fbf68692941
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-07-07 08:06:31 -0500

    add test and feature for us2 parent show

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,4 +2,8 @@ class SectionsController < ApplicationController
     def index 
         @sections = Section.all 
     end
+
+    def show
+        @section = Section.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/parents', to: 'sections#index'
+  get '/parents/:id', to: 'sections#show'
 end
