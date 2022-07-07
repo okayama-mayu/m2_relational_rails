89b0675a16c1d612c65df5ac3d27487125dbf438
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-07-07 12:15:52 -0500

    fix boolean validation for models

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do 
     describe 'data validations' do 
         it { should validate_presence_of :name }
-        # it { should validate_presence_of :need_restock }
         it { should validate_presence_of :price }
+
+        it { should allow_value(true).for(:need_restock) }
+        it { should allow_value(false).for(:need_restock) }
     end
     
     it { should belong_to(:section) }
--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Section, type: :model do 
     describe 'data validations' do 
-        it { should validate_presence_of :name }
-        # it { should validate_presence_of :vegan_options } 
+        it { should validate_presence_of :name } 
         it { should validate_presence_of :labor_intensity }
+        it { should allow_value(true).for(:vegan_options) }
+        it { should allow_value(false).for(:vegan_options) }
     end
 
     it { should have_many :items }    
