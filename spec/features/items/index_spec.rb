8bfce82ef44dc9a51a9d23cb69268890f2420615
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-07-07 20:53:27 -0500

    added Menu Index link at top of Item Index page

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -89,4 +89,59 @@ RSpec.describe 'the child index page', type: :feature do
         expect(page).to have_content(item3.section.name)
         expect(page).to have_content(item4.need_restock)
     end
+
+    # User Story 9, Parent Index Link
+    # As a visitor
+    # When I visit any page on the site
+    # Then I see a link at the top of the page that takes me to the Parent Index
+    it 'shows a link to the Section Index at the top of the page' do
+        Item.destroy_all
+        phillys = Section.create!(name: 'Phillys', vegan_options: false, labor_intensity: 4)
+        vegan_phillys = Section.create!(name: 'Vegan Phillys', vegan_options: true, labor_intensity: 5)
+        sides = Section.create!(name: 'Sides', vegan_options: true, labor_intensity: 3)
+        drinks = Section.create!(name: 'Drinks', vegan_options: true, labor_intensity: 1)
+        kids = Section.create!(name: 'Kids', vegan_options: true, labor_intensity: 2)
+
+        item1 = phillys.items.create!(name: 'Steak Philly', need_restock: false, price: 10)
+
+        item2 = vegan_phillys.items.create!(name: 'Vegan Far East', need_restock: true, price: 15)
+
+        item3 = sides.items.create!(name: 'Pickle Fries', need_restock: false, price: 6)
+        item4 = sides.items.create!(name: 'Vegan Poutine', need_restock: true, price: 10)
+
+        visit '/items'
+        # save_and_open_page 
+
+        within('#top-of-page') do
+            expect(page).to have_link("See All Menu Sections")
+        end
+    end
+
+    it 'allows you to navigate to the Section Index if link is clicked' do 
+        Item.destroy_all
+        Section.destroy_all
+        phillys = Section.create!(name: 'Phillys', vegan_options: false, labor_intensity: 4)
+        vegan_phillys = Section.create!(name: 'Vegan Phillys', vegan_options: true, labor_intensity: 5)
+        sides = Section.create!(name: 'Sides', vegan_options: true, labor_intensity: 3)
+        drinks = Section.create!(name: 'Drinks', vegan_options: true, labor_intensity: 1)
+        kids = Section.create!(name: 'Kids', vegan_options: true, labor_intensity: 2)
+
+        item1 = phillys.items.create!(name: 'Steak Philly', need_restock: false, price: 10)
+
+        item2 = vegan_phillys.items.create!(name: 'Vegan Far East', need_restock: true, price: 15)
+
+        item3 = sides.items.create!(name: 'Pickle Fries', need_restock: false, price: 6)
+        item4 = sides.items.create!(name: 'Vegan Poutine', need_restock: true, price: 10)
+
+        visit "/items"
+        click_link "See All Menu Sections"
+        save_and_open_page
+
+        expect(current_path).to eq('/sections')
+        expect(page).to have_content(phillys.name)
+        expect(page).to have_content(vegan_phillys.name)
+        expect(page).to have_content(sides.name)
+        expect(page).to have_content(drinks.name)
+        expect(page).to have_content(kids.name)
+    end
 end
